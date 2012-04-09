4edc699a5f18fb27012e1947a3646c17e864eb75
Abbie Gonzalez <abbie@abbiecod.es>
2012-04-08 21:34:17 -0300

    Put the symbols in an array.

--- JavaScripts/symbolpop.js
+++ JavaScripts/symbolpop.js
@@ -1,42 +1,42 @@
-// symbolpop.js
-// This is a hack, but needed it to be done quickly
-// as a proof of concept.
+// Accessablize js.
+// This is a hack, but needed it to be done quickly.
 // Send me a round-tuit and i'll fix it
 
-// The whole point is to subtly make symbols stand out
-// to the reader to increase readability.
-// requries, in order: jquery, jquery.highlight-3.js
-// and below just tells it what to highlight.
-// call symbolPop(); from your page to highlight everything.
+   			
 function symbolPop() {
-	$('body').highlight('.');
-   $('body').highlight('\”');
-   $('body').highlight('\"');
-   $('body').highlight('\“');
-	$('body').highlight(',');
-	$('body').highlight(':');
-	$('body').highlight(';');
-	$('body').highlight('(');
-	$('body').highlight(')');
-	$('body').highlight('-');
-	$('body').highlight('[');
-	$('body').highlight(']');
-	$('body').highlight('!');
-	$('body').highlight('@');
-	$('body').highlight('$');
-	$('body').highlight('#');
-	$('body').highlight('%');
-	$('body').highlight('{');
-	$('body').highlight('}');
-	$('body').highlight('<');
-	$('body').highlight('>');
-	$('body').highlight('*');
-	$('body').highlight('^');
-	$('body').highlight('~');
-	$('body').highlight('?');
-	$('body').highlight('/');
-	$('body').highlight('+');
-	$('body').highlight('=');
-	$('body').highlight('|');
-	$('body').highlight('\\');
+    hiliteSymbols = new Array(".",
+                                '\”',
+                                '\"',
+                                '\“',
+                                ",",
+                                ":",
+                                ";",
+                                "(",
+                                ")",
+                                "-",
+                                "[",
+                                "]",
+                                "!",
+                                "@",
+                                "$",
+                                "#",
+                                "%",
+                                "{",
+                                "}",
+                                "<",
+                                ">",
+                                "*",
+                                "^",
+                                "~",
+                                "?",
+                                "/",
+                                "+",
+                                "=",
+                                "|",
+                                '\\');
+                                
+    hiliteSymbols.forEach(function( callback, thisArg ) {
+                                    $('body').highlight(this);
+                                    }
+                                    );
 }
