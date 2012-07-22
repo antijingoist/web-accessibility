6a5552ef8043a55aa872da2ff9d019145ddd95de
Abbie Gonzalez <abbie@abbiecod.es>
2012-07-21 23:46:29 -0400

    not much: erased a blank line.

--- JavaScripts/symbolpop.js
+++ JavaScripts/symbolpop.js
@@ -1,7 +1,6 @@
 // Accessablize js.
 // This is a hack, but needed it to be done quickly.
 // Send me a round-tuit and i'll fix it
-
    			
 function symbolPop() {
     hiliteSymbols = new Array(".",
