633c7f8ad8f5594d20ef5067cdbb69396152c3c0
Abbie Gonzalez <abbie@abbiecod.es>
2015-09-07 11:16:48 -0400

    Fixed chopping up URLs.

--- js/symbolpop.js
+++ js/symbolpop.js
@@ -4,6 +4,6 @@ function symbolPop(theElement) {
 //Will apply a span style to punctuation and capital letters that you can use to make more obvious
    var largeString = "";
 
-   largeString = $(theElement).html().replace(/[A-Z]{1}|!|\.|\,|\?|\$\s?(\d+[\.\s,\dk]+)|(\d+[\.\s,\dk]+)\$s/g, '<span class="symbolPop">$&</span>');
+   largeString = $(theElement).html().replace(/(?:((\<\ba href\b.*\"\>.*\<\/\a\>)))|([A-Z]{1})|!|\.|\,|\?|\$\s?(\d+[\.\s,\dk]+)|(\d+[\.\s,\dk]+)\$s/g, '<span class="symbolPop">$&</span>');
    $(theElement).html(largeString);
 }
