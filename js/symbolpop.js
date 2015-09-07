d73ec96771aeba0a839faa8633ff2b8e01c72e2a
Abbie Gonzalez <abbie@abbiecod.es>
2015-09-07 10:20:36 -0400

    removed dependency on highlight.js

--- js/symbolpop.js
+++ js/symbolpop.js
@@ -1,8 +1,7 @@
 // Accessablize js.
 
-//Will apply a span style to punctuation and capital letters that you can use to make more obvious
 function symbolPop(theElement) {
-   theElement = theElement || "body";
+//Will apply a span style to punctuation and capital letters that you can use to make more obvious
    var largeString = "";
 
    largeString = $(theElement).html().replace(/[A-Z]{1}|!|\.|\,|\?|\$\s?(\d+[\.\s,\dk]+)|(\d+[\.\s,\dk]+)\$s/g, '<span class="symbolPop">$&</span>');
