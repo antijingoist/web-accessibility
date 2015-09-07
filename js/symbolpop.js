702799e3cf730f427536ddc48941433ec5668900
Abbie Gonzalez <abbie@abbiecod.es>
2015-09-07 10:04:23 -0400

    adding optional parameter

--- js/symbolpop.js
+++ js/symbolpop.js
@@ -1,7 +1,8 @@
 // Accessablize js.
 
-function symbolPop(theElement) {
 //Will apply a span style to punctuation and capital letters that you can use to make more obvious
+function symbolPop(theElement) {
+   theElement = theElement || "body";
    var largeString = "";
 
    largeString = $(theElement).html().replace(/[A-Z]{1}|!|\.|\,|\?|\$\s?(\d+[\.\s,\dk]+)|(\d+[\.\s,\dk]+)\$s/g, '<span class="symbolPop">$&</span>');
